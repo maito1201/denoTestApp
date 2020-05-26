564b1befa2eefceed04e5b098622e7df25b82d3e
maito <itomasa0915@gmail.com>
2020-05-26 16:59:28 +0900

    use ts

--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,8 @@
 import { serve } from "https://deno.land/std@0.50.0/http/server.ts";
 import { readFileStr } from 'https://deno.land/std@0.53.0/fs/read_file_str.ts';
 
-const s = serve({ port: 8000 });
-const template = await readFileStr("app/html/hello.html");
+const s: any = serve({ port: 8000 });
+const template: string = await readFileStr("app/html/hello.html");
 console.log("server starting on http://localhost:8000/\nCtrl-C to shutdown container");
 for await (const req of s) {
   req.respond({ body: template });
